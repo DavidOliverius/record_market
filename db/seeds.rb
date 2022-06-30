bdc6027314ad35b115f743d2e2acd80e47eeb103
DavidOliverius <oliveriusdavid@gmail.com>
2022-06-30 12:11:56 +1000

    genres seed added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+genres = [
+    'House', 'Techno', 'Electro', 'Tech House', 'Trance', 'Minimal', 'Breakbeat', 
+    'Progressive House', 'Breaks', 'Garage House', 'Psy-Trance', 'UK Garage', 'Progressive Trance'] 
+
+if Genre.count == 0
+    genres.each do |genre|
+        Genre.create(name: genre)
+        puts "created #{genre} genre"
+    end
+end
