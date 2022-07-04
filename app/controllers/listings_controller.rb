d7600b5309c0f306e4ac93e31573762e199be034
DavidOliverius <oliveriusdavid@gmail.com>
2022-07-04 21:58:26 +1000

    implement local image uploading

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -89,6 +89,6 @@ class ListingsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def listing_params
-      params.require(:listing).permit(:title, :artist, :label, :condition, :description, :price, :sold, :user_id, :genre_id)
+      params.require(:listing).permit(:title, :artist, :label, :condition, :description, :price, :sold, :user_id, :genre_id, :cover)
     end
 end
